a71e8a28f3b72eb2236ca6c1f351f03069d47f8d
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-18 11:54:00 +0100

    layout added to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
+import ToDo from "./compnents/ToDo";
+
 function App() {
   return (
-    <div className="App">
-      <h1>ToDo App</h1>
-      <div className="top">
-        <input type="text" placeholder="add todo"></input>
+    <>
+      <div className="App container">
+        <h1>ToDo App</h1>
+        <div className="top">
+          <input type="text" placeholder="add todo"></input>
+          <div className="add">Add</div>
+        </div>
+        <div className="list">
+          <ToDo text="hi" />
+          <ToDo text="hi" />
+          <ToDo text="hi" />
+          <ToDo text="hi" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
