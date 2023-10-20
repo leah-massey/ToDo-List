fbaeed4873a03bdf029cf95abdb1427687b6a2b7
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-20 19:56:47 +0100

    refactor

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="flex align-center text-center pb-3 mt-auto font-mono text-sm">
-      <a href="https://github.com/leah-massey/simpletodo" target="_blank">
+      <a href="https://github.com/leah-massey/simpletodo">
         Take a look at my code on Github
       </a>
     </div>
