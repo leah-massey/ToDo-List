cfb0b7a526991b6b88982ca15195c0549d4a19f7
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-26 12:23:59 +0100

    style: updates for mobile use

--- frontend/src/components/ToDo.js
+++ frontend/src/components/ToDo.js
@@ -5,8 +5,8 @@ import { BiEdit } from "react-icons/bi";
 const ToDo = ({ text, updateMode, deleteToDo }) => {
   return (
     <div className="todo bg-blue-100  ">
-      <div className="text ">{text}</div>
-      <div className="icons">
+      <div className="text w-3/5 left-3 ">{text}</div>
+      <div className="icons right-3">
         <BiEdit className="icon " onClick={updateMode} />
         <AiFillDelete className="icon " onClick={deleteToDo} />
       </div>
